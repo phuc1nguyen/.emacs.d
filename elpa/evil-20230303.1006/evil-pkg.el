905b804e7cb3499c859daaedf5a5eee0d6bc456f
phuc1nguyen <phuc.ng13988@gmail.com>
2023-03-04 19:16:57 +0700

    Add extensible vi layer package (evil)

---
+++ elpa/evil-20230303.1006/evil-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil" "20230303.1006" "Extensible Vi layer for Emacs."
+  '((emacs "24.1")
+    (goto-chg "1.6")
+    (cl-lib "0.5"))
+  :commit "96280419b46d43f5b7c33e3c655b62da69e0c671" :maintainer
+  '("Tom Dalziel" . "tom.dalziel@gmail.com")
+  :keywords
+  '("emulations")
+  :url "https://github.com/emacs-evil/evil")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
